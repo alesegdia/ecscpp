228d8111c14a434d41a9fc4df9680498e58b12b5
Alejandro Seguí <alesegdia@gmail.com>
2015-01-27 23:00:58 +0100

    Update and set frames for Animation.

--- src/lib/core/assets/Animation.cpp
+++ src/lib/core/assets/Animation.cpp
@@ -22,6 +22,12 @@ bool Animation::Prepare( int num_frames, int num_plays )
 	return retcode;
 }
 
+void Animation::SetFrame( int frame_seq, int frame_sheet, sf::Time duration )
+{
+	_frames[frame_seq].frameIndex = frame_sheet;
+	_frames[frame_seq].frameDuration = duration;
+}
+
 void Animation::Reset( Animation::Data& data )
 {
 	data.currentFrame = 0;
@@ -30,8 +36,37 @@ void Animation::Reset( Animation::Data& data )
 	data.lastPlay = 0;
 }
 
-void Animation::Update( sf::Time& delta, Animation::Data& data )
+void Animation::Update( sf::Time& delta, Animation::Data& d )
 {
+	bool play = true;
+	sf::Time rest = d.currentFrameTimer - delta;
+	// if we have to change to reset the animation
+	if( rest < sf::Time::Zero && d.currentFrame == _frames.size() - 1 )
+	{
+		// keep playing if infinite, stop if not
+		play = d.numPlays < 0;
 
+		// but if there are still plays, we compute it
+		if( d.numPlays > 0 )
+		{
+			// we finished this play, next!
+			d.numPlays--;
+			d.currentFrame = 0;
+			d.currentFrameTimer = _frames[0].frameDuration - rest;
+		}
+	}
+
+	if( play )
+	{
+		if( rest < sf::Time::Zero )
+		{
+			d.currentFrame = (d.currentFrame + 1) % _frames.size();
+			d.currentFrameTimer = -rest;
+		}
+		else
+		{
+			d.currentFrameTimer -= rest;
+		}
+	}
 }
 
--- src/lib/core/assets/Animation.h
+++ src/lib/core/assets/Animation.h
@@ -9,7 +9,7 @@ class Animation {
 public:
 
 	struct Data {
-		int currentFrame; 				// index of the current running frame
+		size_t currentFrame; 				// index of the current running frame
 		sf::Time currentFrameTimer; 	// time that the current frame has been running
 		int lastPlay; 					// is the last time this animation is played
 		int numPlays; 					// number of times the animation will be played
@@ -24,7 +24,7 @@ public:
 	virtual ~Animation ();
 
 	bool Prepare( int num_frames, int num_plays );
-	void SetFrame( int frame_seq, int frame_sheet, float duration );
+	void SetFrame( int frame_seq, int frame_sheet, sf::Time duration );
 
 	void Reset( Animation::Data& data );
 	void Update( sf::Time& delta, Animation::Data& data );
