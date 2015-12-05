4eb2680dc6e3e7e39b2539bae77f72bbd58867c1
Alejandro Seguí <alesegdia@gmail.com>
2015-12-05 18:33:20 +0100

    removing old sample config

--- premake4.lua
+++ premake4.lua
@@ -50,22 +50,3 @@ for file in lfs.dir([[src/tests]]) do
 	end
 end
 
---[[
-  links		{ "glrayfw", "c", "dl", "m", "rt", "freetype", "SDL2", "Box2D", "GL" }
-  buildoptions { "-std=c++11", "-pg" }
-  includedirs { "~/Libs/cml-1_0_3/" }
-  configuration { "Debug*" }
-	defines	{ "_DEBUG", "DEBUG" }
-	flags	{ "Symbols" }
-	targetdir "bin/debug64"
-  configuration { "not Debug*" }
-	defines { "NDEBUG" }
-	flags	{ "Optimize" }
-	targetdir "bin/release64"
-  configuration { "x32" }
-	includedirs { "lib/glrayfw", "include32", "/usr/include/freetype2" }
-	linkoptions { "-pg" }
-  configuration { "x64" }
-	includedirs { "lib/glrayfw", "include64,include","/usr/include/freetype2" }
-	linkoptions { "-pg", "-ftest-coverage", "-fprofile-arcs" }
---]]
