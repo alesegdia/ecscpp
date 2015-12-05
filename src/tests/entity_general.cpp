0a244297c5082cc065354b8d9ed2f5155949b7c8
Alejandro Seguí <alesegdia@gmail.com>
2015-12-05 19:56:13 +0100

    removing print output from entity_general test

--- src/tests/entity_general.cpp
+++ src/tests/entity_general.cpp
@@ -6,6 +6,5 @@ int main( int argc, char** argv ) {
 	Entity e;
 	e.setEID(12345);
 	assert(e.getEID() == 12345 && "getEID or setEID is not working");
-	std::cout << "getEID and setEID pass!\n";
 	return 0;
 }
