63d4d10e4bdaad4b2f5cf8925379eb44c4aa5eec
Alejandro Seguí <alesegdia@gmail.com>
2015-12-08 18:58:24 +0100

    adding bits builder to traits

--- src/lib/ecs/component/ComponentTraits.h
+++ src/lib/ecs/component/ComponentTraits.h
@@ -10,14 +10,17 @@ typedef std::uint32_t ctindex_t;
 class ComponentTraits
 {
 public:
-	template <typename T>
-	static ctflags_t GetFlag() {
+
+    template <typename T>
+    static ctflags_t GetFlag()
+    {
 		static const ctflags_t flag = pow(2, ComponentTraits::GetIndex<T>());
 		return flag;
 	}
 
 	template <typename T>
-	static ctflags_t GetIndex() {
+    static ctflags_t GetIndex()
+    {
 		static const ctindex_t index = nextTypeIndex++;
 		return index;
 	}
@@ -26,8 +29,36 @@ public:
 		return nextTypeIndex;
 	}
 
+    template <typename... Args>
+    static ctflags_t BuildBits() {
+        return ComponentBitsBuilder<Args...>::buildBits();
+    }
+
+
 private:
 	static ctflags_t nextTypeIndex;
+
+    template <typename... Rest>
+    struct ComponentBitsBuilder;
+
+    template <typename First, typename... Rest>
+    struct ComponentBitsBuilder<First, Rest...>
+    {
+        static ctflags_t buildBits()
+        {
+            return GetFlag<First>() | ComponentBitsBuilder<Rest...>::buildBits();
+        }
+    };
+
+
 };
 
+template <>
+struct ComponentTraits::ComponentBitsBuilder<>
+{
+    static ctflags_t buildBits()
+    {
+        return 0;
+    }
+};
 
