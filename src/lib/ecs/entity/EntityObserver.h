f628cbc30152057780bc7787d2d40e729bed9474
Alejandro Seguí <alesegdia@gmail.com>
2015-12-18 01:01:08 +0100

    renaming added/deleted to entityAdded/entityDeleted; adding entityChanged; documenting with doxygen

--- src/lib/ecs/entity/EntityObserver.h
+++ src/lib/ecs/entity/EntityObserver.h
@@ -2,16 +2,33 @@
 #define __ENTITYOBSERVER_H__
 
 #include "Entity.h"
-#include "eid_t.h"
 
-class EntityObserver
+/**
+ * @brief The IEntityObserver interface is used for an object that needs
+ * to be notified about entity operations
+ */
+class IEntityObserver
 {
 public:
-	virtual ~EntityObserver() {	}
+	virtual ~IEntityObserver() {	}
 
-	/* Observer interface */
-	virtual void added(eid_t e)	{ }
-	virtual void deleted(eid_t e) {	}
+	/**
+	 * @brief called when an entity has been added to the world
+	 * @param e the entity
+	 */
+	virtual void entityAdded(Entity* e) = 0 ;
+
+	/**
+	 * @brief called when an entity has been deleted from the world
+	 * @param e the entity
+	 */
+	virtual void entityDeleted(Entity* e) = 0 ;
+
+	/**
+	 * @brief called when a component has been attached to or removed from an entity
+	 * @param e the entity
+	 */
+	virtual void entityChanged(Entity* e) = 0 ;
 };
 
 #endif
